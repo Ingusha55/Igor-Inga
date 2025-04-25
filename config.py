5ce51613c86801860d9f7b66376be246c8c3c7c4
Ingusha55 <yashar.ismailov19699@gmail.com>
2025-04-25 12:50:12 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
+CHAT_ID = os.getenv("CHAT_ID")
+API_ID = int(os.getenv("API_ID"))
+API_HASH = os.getenv("API_HASH")
